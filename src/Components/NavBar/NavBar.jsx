610e3106fd5ea6c9c61cc5f19da6e1f751d4534a
brunosarti83 <brunosarti.bs@gmail.com>
2023-11-22 09:20:07 -0300

    WIP(NavBar Links)

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,20 @@
-// https://designshack.net/articles/css/inner-shadows-in-css-images-text-and-beyond/
+// for name inset: https://designshack.net/articles/css/inner-shadows-in-css-images-text-and-beyond/
 
+import { useNavigate } from "react-router-dom";
 import SlashingButton from "../SlashingButton/SlashingButton";
+import { RoutesHelper } from "../../utils/routes";
+import { useState } from "react";
 
 export default function NavBar() {
+  const navigate = useNavigate();
+  const [section, setSection] = useState(RoutesHelper.home); // could this be Ref ? Would it render ?
+  const onClick = (to) => {
+    if (to !== section) {
+      setSection(to);
+      navigate(to, { state: { navBarSays: to } });
+    }
+  };
+
   return (
     <div className="w-full h-[90px] bg-white flex align-center sticky top-0 left-0 px-10 z-20">
       <h1 className="font-work font-extrabold text-[22px] self-center ml-2 bg-gray-500 text-transparent [text-shadow:_0_2px_3px_rgba(255,_255,_255,_0.7)] bg-clip-text">
@@ -10,8 +22,20 @@ export default function NavBar() {
       </h1>
       <div className="w-1/2 self-center text-sm ml-auto">
         <ul className="flex flex-row justify-around">
-          <li className="font-source font-semibold my-auto">About me</li>
-          <li className="font-source font-semibold my-auto">Projects</li>
+          <li
+            className={
+              "font-source font-semibold my-auto hover:cursor-pointer" + `${""}`
+            }
+            onClick={() => onClick(RoutesHelper.home)}
+          >
+            About me
+          </li>
+          <li
+            className="font-source font-semibold my-auto hover:cursor-pointer"
+            onClick={() => onClick(RoutesHelper.projects)}
+          >
+            Projects
+          </li>
           <li className="font-source font-semibold my-auto">
             <SlashingButton
               text="Download CV"
@@ -19,7 +43,12 @@ export default function NavBar() {
               secondary="white"
             />
           </li>
-          <li className="font-source font-semibold my-auto">Feedback</li>
+          <li
+            className="font-source font-semibold my-auto hover:cursor-pointer"
+            onClick={() => onClick(RoutesHelper.feedback)}
+          >
+            Feedback
+          </li>
         </ul>
       </div>
     </div>
--- src/Views/Projects/Projects.jsx
+++ src/Views/Projects/Projects.jsx
@@ -1,15 +1,23 @@
 import { projects } from "../../db";
 import Project from "../../Components/Project/Project";
+import { useEffect, useRef } from "react";
 
 export default function Projects() {
+  const firstObjectBottom = useRef(null);
+  useEffect(() => {
+    firstObjectBottom.current.scrollIntoView(false);
+  }, []);
   return (
     <div className="flex flex-col min-h-[calc(100%_-_90px)]">
       {projects.map((project, index) => (
-        <Project
-          key={project.name}
-          id={projects.length - index}
-          project={project}
-        />
+        <div key={project.name}>
+          <Project
+            key={project.name}
+            id={projects.length - index}
+            project={project}
+          />
+          {index === 0 && <div ref={firstObjectBottom} className="-mt-2"></div>}
+        </div>
       ))}
     </div>
   );
