6b38e00203a3002f35b3c3dde8f3f9406a7054a7
brunosarti83 <brunosarti.bs@gmail.com>
2023-11-24 12:45:05 -0300

    feat(utils/postAnswer.js with validation)

--- src/utils/postAnswer.js
+++ src/utils/postAnswer.js
@@ -1,30 +1,18 @@
 import axios from "axios";
 import { getBaseToken, postNewEntry } from "./seaTableFoo.js";
 import { api_token } from "../db.js";
+import { validateRow } from "./validateRow.js";
 
 // MISSING ALL VALIDATIONS
 export const postAnswer = async ({ row, table_name }) => {
     try {
+        validateRow(row)
         const baseData = await getBaseToken(api_token)
         const {access_token:base_token, dtable_uuid:base_uuid} = baseData
         const response = await postNewEntry({ base_token, base_uuid, row, table_name })
         return response
 
     } catch (error) {
-        throw new Error({message: error.message})
+        throw new Error(error.message)
     }
 }
-
-const row = {
-    Q1_pos1:"React", 
-    Q1_pos2:"Node", 
-    Q1_pos3:"Typescript", 
-    Q1_pos4:"CSS / Styling", 
-    Q1_pos5:"Databases", 
-    Q2_options: ["Angular"], 
-    Q2_others: ""
-}
-
-const table_name = 'Answers'
-const result = await postAnswer({row, table_name})
-console.log(result)
--- src/utils/validateRow.js
+++ src/utils/validateRow.js
@@ -13,34 +13,21 @@ export const validateRow = (row) => {
     const Q1 = [Q1_pos1, Q1_pos2, Q1_pos3, Q1_pos4, Q1_pos5]
     Q1.forEach(q => {
         if (q && !questions.skills.includes(q)) {
-            throw new Error({message: `${q} is not a valid option`})
+            throw new Error(`${q} is not a valid option for question 1`)
         }
     })
     const Q1Set = new Set(Q1)
-    if (Q1Set.length < Q1.length) {
-        throw new Error({message: `Some of the options on Q1 are repeated`})
+    if (Q1Set.size < Q1.length) {
+        throw new Error(`Some of the options on Q1 are repeated`)
     }
+    if (!Array.isArray(Q2_options)) {
+        throw new Error(`Q2_options must be an array`)
+    }
+    Q2_options.forEach(opt => {
+        if (!questions.not.includes(opt)) {
+            throw new Error(`${opt} is not a valid option for question 2`)
+        }
+    })
+    return 'OK'
 }
 
-
-
-
-
-
-
-
-
-
-
-
-const row = {
-    Q1_pos1:"React", 
-    Q1_pos2:"Node", 
-    Q1_pos3:"Typescript", 
-    Q1_pos4:"CSS / Styling", 
-    Q1_pos5:"Databases", 
-    Q2_options: ["Angular"], 
-    Q2_others: ""
-}
-
-const table_name = 'Answers'
