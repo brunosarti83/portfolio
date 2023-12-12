5e1e2784bd692225df9715b9a93dbe7b99fd03f8
brunosarti83 <brunosarti.bs@gmail.com>
2023-12-12 15:52:31 -0300

    fix(About/ added fullstack developer)

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,14 +7,14 @@ export default function About() {
   return (
     <div className="h-[calc(100%_-_90px)] w-full flex flex-col gap-5 bg-gradient-to-t from-red-100 to-[#FF0000] to-15% md:px-20">
       <div className="md:h-[80%] md:max-w-[95%] flex max-md:flex-col pt-4 md:justify-center gap-4 md:m-auto">
-        <div className="w-[85%] md:w-[50%] h-full bg-yellow-400 flex md:pl-16 md:pr-24 ml-auto font-source text-[2vh] md:text-[1.8vh] rounded-bl-3xl overflow-y-scroll scrollbar-none max-md:py-8">
+        <div className="w-[85%] md:w-[50%] h-full bg-yellow-400 flex md:pl-16 md:pr-20 ml-auto font-source text-[2vh] md:text-[1.8vh] rounded-bl-3xl overflow-y-scroll scrollbar-none max-md:py-8">
           <div className="m-auto pt-2">
             <span className="font-source leading-5 shadowed-text">
               Hi, i’m a recently graduated
             </span>
             <br />
             <span className="font-source leading-5 shadowed-text">
-              developer from Argentina,
+              fullstack developer from Argentina,
             </span>
             <br />
             <span className="font-source leading-5 shadowed-text">
