a008d4c559cec02ab44439e73fef83045fd6421e
brunosarti83 <brunosarti.bs@gmail.com>
2023-12-14 13:39:04 -0300

    feat(SVG text entry animation)

--- src/Components/SvgText/SvgText.jsx
+++ src/Components/SvgText/SvgText.jsx
@@ -1,5 +1,6 @@
 // https://www.youtube.com/watch?v=5Fxjt5ZNC00
 import video from "../../assets/pexels_videos_1.mp4";
+import { motion } from "framer-motion";
 
 export default function SvgText() {
   return (
@@ -13,9 +14,47 @@ export default function SvgText() {
         className="w-full h-full object-fill relative max-md:bottom-20 -scale-x-100"
       ></video>
       <div className="w-full font-work font-extrabold text-[7rem] leading-[6rem] md:text-[160px] md:leading-[130px] tracking-tighter text-white absolute top-0 left-0 h-full bg-black mix-blend-multiply px-10">
-        <h1 className="mt-[100px] md:mt-5">Full</h1>
-        <h1>Stack</h1>
-        <h1 className="break-all">Developer</h1>
+        <motion.h1
+          initial={{
+            x: "-500px",
+          }}
+          animate={{
+            x: "0px",
+          }}
+          transition={{
+            duration: "1",
+          }}
+          className="mt-[100px] md:mt-5"
+        >
+          Full
+        </motion.h1>
+        <motion.h1
+          initial={{
+            x: "1000px",
+          }}
+          animate={{
+            x: "0px",
+          }}
+          transition={{
+            duration: "1",
+          }}
+        >
+          Stack
+        </motion.h1>
+        <motion.h1
+          initial={{
+            x: "-500px",
+          }}
+          animate={{
+            x: "0px",
+          }}
+          transition={{
+            duration: "1",
+          }}
+          className="break-all"
+        >
+          Developer
+        </motion.h1>
       </div>
     </div>
   );
