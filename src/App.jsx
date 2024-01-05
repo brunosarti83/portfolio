e8da13fc09f4c94eba64a0455a1ec4af3d33c6a9
brunosarti83 <brunosarti.bs@gmail.com>
2024-01-05 17:39:03 -0300

    mobile bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import BottomBar from "./Components/BottomBar/BottomBar";
 
 function App() {
   return (
-    <div className="w-screen md:w-screen md:h-screen bg-gradient-to-tl from-slate-200 to-[#00FFB2] via-sky-200 flex">
-      <div className="w-[98%] md:w-5/6 max-w-[1000px] m-auto flex flex-col gap-4">
+    <div className="w-full overflow-x-hidden md:w-screen md:h-screen bg-gradient-to-tl from-slate-200 to-[#00FFB2] via-sky-200 flex">
+      <div className="w-[98%] md:w-5/6 md:max-w-[1000px] m-auto flex flex-col gap-4">
         <div className="w-full h-full flex md:flex-row gap-2 mt-1 m-auto flex-col">
           <Display />
           <SideBar />
